9936763a973a07ed2acc4a6a0b7a6befeef8f37e
dorian921 <144111488+dorian921@users.noreply.github.com>
2023-10-26 16:22:16 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,23 @@
+
+let  budget = 50;
+        let product = 60;
+
+        if (budget >= 60) {
+            
+            
+            console.log ('u heeft genoeg geld')
+        } else {
+        
+            console.log ('helaas u heeft te weinig geld')
+        }
+
+
+
+
+
+
+
+
+
+
+
